480d02c52dfa562264bf706772bbccdb7041133c
romulomoura <ferreira.moura.maia@gmail.com>
2018-12-07 00:03:40 -0300

    definindo funcionalidades app

---
+++ app.js
@@ -0,0 +1,69 @@
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice(){
+    const choices = ['r', 'p', 's'];
+    const randomNumber = (Math.floor(Math.random() * 3));
+    return choices[randomNumber];
+}
+
+function win(user, computer){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_div.innerHTML = user + " beats " + computer + ", You Win";
+}
+
+function lose() {
+    console.log("LOSE!");
+    console.log(userScore);
+}
+
+function draw(){
+    console.log("DRAW!");
+    console.log(userScore);
+}
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice); 
+            break;
+        case "sr":
+        case "ps":
+        case "rp":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
+    }
+}
+
+function main(){
+    rock_div.addEventListener('click', function(){
+        game("r");
+    })
+
+    paper_div.addEventListener('click', function(){
+        game("p");
+    })
+
+    scissors_div.addEventListener('click', function(){
+        game("s");
+    })
+}
+
+main();
